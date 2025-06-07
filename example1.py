f3bf5bf422f350c62ecf79ec32725ae7cba106de
felipe <johagalvis441@gmail.com>
2025-06-06 21:33:45 -0500

    Script: data types in python

---
+++ example1.py
@@ -0,0 +1,23 @@
+# this is an example with python 
+'''
+Date:06-jun-2025
+dev: felipe galvis
+'''
+data1 = 10
+data2 =3,50
+data3 = "felipe"
+data4 = "galvis"
+data5 = '''
+Hello people, we are learning python
+That's wonderful 
+'''
+data6 = True
+data7 = 3,4
+ 
+print(type(data1))
+print(type(data2))
+print(type(data3))
+print(type(data4))
+print(type(data5))
+print(type(data6))
+print(type(data7))
